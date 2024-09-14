652c9c945da8f21aed44ce974054507956cc5603
unknown <dyusmirnov@stud.etu.ru>
2024-09-14 22:11:04 +0300

    feat: logger package

---
+++ hello_world/pkg/logger/logger.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "production"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return logger
+}
