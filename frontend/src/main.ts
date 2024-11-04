c799d07150800ffdbc0cc580b27fc0dba11f2f99
1that <georgiOlio@yandex.ru>
2024-11-04 23:32:17 +0300

    change .gitignore and add vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,25 @@
-import { createApp } from 'vue'
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// pinia
 import { createPinia } from 'pinia'
+
+import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routing/router';
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(pinia)
 app.use(router)
 
