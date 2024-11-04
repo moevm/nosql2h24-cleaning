108ff301cf043cea6867e7c86c252eb2c27e2880
unknown <dyusmirnov@stud.etu.ru>
2024-11-04 23:59:41 +0300

    feat: base config

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/moevm/nosql2h24-cleaning/cleaning/internal/app"
+	"github.com/moevm/nosql2h24-cleaning/cleaning/internal/config"
+)
 
+func main() {
+	cfg := config.MustLoad()
+	app.Run(cfg)
 }
---
+++ backend/internal/app/app.go
@@ -0,0 +1,9 @@
+package app
+
+import (
+	"github.com/moevm/nosql2h24-cleaning/cleaning/internal/config"
+)
+
+func Run(cfg *config.Config) {
+	_ = cfg
+}
