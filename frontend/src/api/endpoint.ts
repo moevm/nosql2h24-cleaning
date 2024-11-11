102a746ed89c38752acb0bdaff2a4ca2bec2bc05
1that <georgiOlio@yandex.ru>
2024-11-12 00:08:42 +0300

    create register request

--- frontend/src/api/endpoint.ts
+++ frontend/src/api/endpoint.ts
@@ -1,5 +1,9 @@
-export const getClientAddressesPath = (id: number) => `/users/${id}/addresses`;
-export const createNewAddressPath = (id: number) => `/users/${id}/addresses`
-export const getClientAddressPath = (id: number, address_id: number) => `/users/${id}/addresses/${address_id}`
-export const updateClientAddressPath = (id: number, address_id: number) => `/users/${id}/addresses/${address_id}`
-export const deleteClientAddressPath = (id: number, address_id: number) => `/users/${id}/addresses/${address_id}`
+export const getLoginPath = '/auth/login'
+export const getLogoutPath = '/auth/logout'
+export const getRegisterPath = '/auth/register'
+
+export const getClientAddressesPath = (id: number) => `/v1/users/${id}/addresses`
+export const createNewAddressPath = (id: number) => `/v1/users/${id}/addresses`
+export const getClientAddressPath = (id: number, address_id: number) => `/v1/users/${id}/addresses/${address_id}`
+export const updateClientAddressPath = (id: number, address_id: number) => `/v1/users/${id}/addresses/${address_id}`
+export const deleteClientAddressPath = (id: number, address_id: number) => `/v1/users/${id}/addresses/${address_id}`
