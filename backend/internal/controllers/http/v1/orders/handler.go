d48a1350334651015eccacee54bc24ca06bfcbbe
unknown <dyusmirnov@stud.etu.ru>
2024-11-16 14:08:12 +0300

    feat: endpoints & docs for orders

---
+++ backend/internal/controllers/http/v1/orders/handler.go
@@ -0,0 +1,29 @@
+package orders
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/moevm/nosql2h24-cleaning/cleaning/internal/services"
+)
+
+type Handler struct {
+	service *services.OrderService
+}
+
+func NewHandler(service *services.OrderService) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) Routes() chi.Router {
+	router := chi.NewRouter()
+
+	router.Get("/", h.GetOrders)
+	router.Post("/", h.CreateOrder)
+
+	router.Route("/{id}", func(r chi.Router) {
+		r.Get("/", h.GetOrder)
+		r.Put("/", h.UpdateOrder)
+		r.Delete("/", h.DeleteOrder)
+	})
+
+	return router
+}
