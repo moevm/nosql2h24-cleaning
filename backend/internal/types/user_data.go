aa47c4a99dcdd0fdf5fc15e1566e83710e2aaecb
unknown <dyusmirnov@stud.etu.ru>
2024-12-19 22:46:59 +0300

    feat: add phone_number to UserData

--- backend/internal/types/user_data.go
+++ backend/internal/types/user_data.go
@@ -1,8 +1,9 @@
 package types
 
 type UserData struct {
-	Name       string `json:"name"`
-	Surname    string `json:"surname"`
-	Patronymic string `json:"patronymic"`
-	Email      string `json:"email"`
+	Name        string `json:"name"`
+	Surname     string `json:"surname"`
+	Patronymic  string `json:"patronymic"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
 }
